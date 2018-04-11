dc595b9b011081d032b392a5635374a4ebaa6965
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2018-04-11 20:23:38 +0300

    update weather.js

--- weather.js
+++ weather.js
@@ -1,5 +1,5 @@
 const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather?';
-const API_KEY = 'e2a9d3bbc82b867e97b63c4481b5ebac';
+const API_KEY = 'YOUR_KEY_HERE';
 
 const WeatherService = {
 
@@ -28,7 +28,7 @@ const WeatherService = {
 		}
 
 		return {
-			isRaining,
+			isRaining: true,
 			description: weatherData.weather[0].description,
 			iconUrl: 'http://openweathermap.org/img/w/' + weatherData.weather[0].icon + '.png'
 		}
